6a94986945788064ff3e9f0779446ba9b5f22679
Ben Smerd <bsmerd@adam.com.au>
2023-01-12 19:01:40 +1030

    init commit

---
+++ main.py
@@ -0,0 +1,3 @@
+#  Call it EasyReddit- for orgammerrs who are bored while in the terminal and want to have a quick look on reddit
+
+# ? a python app where user inputs a keyword and then all the subreddits with that keyword are shown for the user to click on and then the top rated in descending order get shown--- very simple but good use of API for python
